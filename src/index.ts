7b1e7c1eec4e98463d2a27a5974327c7dfb444d8
lz82 <nickylau82@gmail.com>
2020-11-19 23:39:25 +0800

    :constructon: modify filename to hash value

--- src/index.ts
+++ src/index.ts
@@ -14,39 +14,38 @@ const app = new Koa();
 const router = new Router();
 
 app.use(
-	bodyParser({
-		multipart: true,
-	})
+  bodyParser({
+    multipart: true,
+  })
 );
 app.use(cors());
 router.get('/', async (ctx) => {
-	ctx.body = `hello world`;
+  ctx.body = `hello world`;
 });
 
 router.post('/upload', async (ctx, next) => {
-	const { hash, order, filename } = ctx.request.body;
-	const chunk = ctx.request.files?.chunk;
+  const { hash, order, filename } = ctx.request.body;
+  const chunk = ctx.request.files?.chunk;
 
-	const chunkDir = path.resolve(UPLOAD_DIR, filename);
-	if (!fse.existsSync(UPLOAD_DIR)) {
-		await fse.mkdir(UPLOAD_DIR);
-	}
-	if (!fse.existsSync(chunkDir)) {
-		await fse.mkdir(chunkDir);
-	}
-	if (chunk?.path) {
-		await fse.move(chunk.path, `${chunkDir}/${hash}`);
-	}
-	console.log(hash + 'upload complete');
-	ctx.body = `received file chunk...`;
+  const chunkDir = path.resolve(UPLOAD_DIR, filename);
+  if (!fse.existsSync(UPLOAD_DIR)) {
+    await fse.mkdir(UPLOAD_DIR);
+  }
+  if (!fse.existsSync(chunkDir)) {
+    await fse.mkdir(chunkDir);
+  }
+  if (chunk?.path) {
+    await fse.move(chunk.path, `${chunkDir}/${hash}`);
+  }
+  console.log(hash + 'upload complete');
+  ctx.body = `received file chunk...`;
 });
 
 router.post('/merge', async (ctx) => {
-	const { filename, size } = ctx.request.body;
-	console.log(filename, size);
-	const filePath = path.resolve(SAVE_DIR, `${filename}`);
-	mergeFileChunk(UPLOAD_DIR, filePath, filename, size);
-	ctx.body = `merge...`;
+  const { filename, fileHash, size } = ctx.request.body;
+  const filePath = path.resolve(SAVE_DIR, `${filename}`);
+  mergeFileChunk(UPLOAD_DIR, filePath, fileHash, size);
+  ctx.body = `merge...`;
 });
 
 app.use(router.routes());
--- src/utils/file.ts
+++ src/utils/file.ts
@@ -5,44 +5,44 @@ import fse, { WriteStream } from 'fs-extra';
 const extractExt = (filename: string) => filename.slice(filename.lastIndexOf('.'), filename.length);
 
 const pipeStream = (path: string, writeStream: WriteStream) => {
-	return new Promise((resolve) => {
-		try {
-			const readStream = fse.createReadStream(path);
-			console.log(writeStream);
-			readStream.on('end', () => {
-				console.log('read end...');
-				fse.unlinkSync(path);
-				resolve('stream read end...');
-			});
-			readStream.pipe(writeStream);
-		} catch (err) {
-			console.log(err);
-		}
-	});
+  return new Promise((resolve) => {
+    try {
+      const readStream = fse.createReadStream(path);
+      console.log(writeStream);
+      readStream.on('end', () => {
+        console.log('read end...');
+        // fse.unlinkSync(path);
+        resolve('stream read end...');
+      });
+      readStream.pipe(writeStream);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 };
 
-export async function mergeFileChunk(dir: string, filePath: string, filename: string, size: number) {
-	const chunkDir = path.resolve(dir, filename);
-	try {
-		const chunkPath: string[] = await fse.readdir(chunkDir);
-		chunkPath.sort((a, b) => ((a.split('-')[1] as unknown) as number) - ((b.split('-')[1] as unknown) as number));
-		console.log(filePath);
-		await Promise.all(
-			chunkPath.map((cp, index) => {
-				pipeStream(
-					path.resolve(chunkDir, cp),
-					fs.createWriteStream(filePath, {
-						start: index * size,
-					})
-				);
-			})
-		);
-		try {
-			fse.rmdirSync(chunkDir);
-		} catch (err) {
-			console.log(err);
-		}
-	} catch (err) {
-		console.log(err);
-	}
+export async function mergeFileChunk(dir: string, filePath: string, fileHash: string, size: number) {
+  const chunkDir = path.resolve(dir, fileHash);
+  try {
+    const chunkPath: string[] = await fse.readdir(chunkDir);
+    chunkPath.sort((a, b) => ((a.split('-')[1] as unknown) as number) - ((b.split('-')[1] as unknown) as number));
+    console.log(filePath);
+    await Promise.all(
+      chunkPath.map((cp, index) => {
+        pipeStream(
+          path.resolve(chunkDir, cp),
+          fs.createWriteStream(filePath, {
+            start: index * size,
+          })
+        );
+      })
+    );
+    try {
+      // fse.rmdirSync(chunkDir);
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
